57eee80b6f52e3d875f9b800ea896e39e285849e
student <harishharish2793@gmail.com>
2024-02-16 10:38:59 +0530

    first commit

---
+++ src/components/Subscribers/Subscribers.js
@@ -0,0 +1,62 @@
+import "./Subscribers.css";
+import React from "react";
+const Subscribers = () => {
+
+
+  return (
+    <>
+    <br/>
+    <table className="subtable">
+<tr>
+    <th>Name</th>
+    <th>Mobile</th>
+    <th>Gmail</th>
+</tr>
+<tr>
+    <td>vimal</td>
+    <td>986541357</td>
+    <td>dummy@gmail.com</td>
+</tr>
+<tr>
+    <td>Kamal</td>
+    <td>986541357</td>
+    <td>dummy@gmail.com</td>
+</tr>
+<tr>
+    <td>Harish</td>
+    <td>986541357</td>
+    <td>dummy@gmail.com</td>
+</tr>
+<tr>
+    <td>Ramesh</td>
+    <td>986541357</td>
+    <td>dummy@gmail.com</td>
+</tr>
+<tr>
+    <td>Haritha</td>
+    <td>986541357</td>
+    <td>dummy@gmail.com</td>
+</tr>
+<tr>
+    <td>Suresh</td>
+    <td>986541357</td>
+    <td>dummy@gmail.com</td>
+</tr>
+<tr>
+    <td>Vimelesh</td>
+    <td>986541357</td>
+    <td>dummy@gmail.com</td>
+</tr>
+<tr>
+    <td>shalini</td>
+    <td>986541357</td>
+    <td>dummy@gmail.com</td>
+</tr>
+
+    </table>
+    </>
+  );
+};
+
+export default Subscribers;
+
